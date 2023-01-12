bc8e6c78f96bf7f41da7e121760f4cee9eb0836d
Marcilio Freitas <marcilio@beltsistemas.com.br>
2023-01-12 15:50:31 -0300

    Modularização: sepração do layout de inicio e o layout de acesso.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,14 +5,25 @@ import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { CadastrarDispositivosComponent } from './cadastrar-dispositivos/cadastrar-dispositivos.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LayoutAcessoComponent } from './layout-acesso/layout-acesso.component';
+import { LayoutInicioComponent } from './layout-inicio/layout-inicio.component';
 
 const routes: Routes = [
-  {path: 'inicio', component: InicioComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'cadastro', component: CadastroComponent},
+  {path: 'inicio', component: LayoutInicioComponent,
+    children: [
+      {path: 'acao', component: InicioComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'cadastro', component: CadastroComponent},
+    ]
+  },
   {path: '', redirectTo: '/inicio', pathMatch: 'full'},
-  {path: 'dispositivos', component: CadastrarDispositivosComponent},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'acesso', component: LayoutAcessoComponent,
+    children: [
+      {path: 'cadastro-dispositivo', component: CadastrarDispositivosComponent},
+      {path: 'dashboard', component: DashboardComponent}
+    ]
+  },
+
 ];
 
 @NgModule({
